d561b2538a479d581b2b1d42d9b13dae30d33e13
VishalKumarSain <vishaldhoria007@gmail.com>
2024-08-06 23:09:54 +0530

    folder changes

--- controllers/folder.controller.js
+++ controllers/folder.controller.js
@@ -1,26 +1,30 @@
-const { isValidObjectId } = require("mongoose");
+const { isValidObjectId, Types } = require("mongoose");
 const { find_one_folder, add_new_folder, find_folders, delete_one_folder } = require("../services/folder.services");
 
 exports.create_folder = async (req, res) => {
   try {
-    const user_id = req.user_id
+    const user_id = req.user_id;
     const { folder_name, parent_folder } = req.body;
     if (!folder_name) return res.status(400).json({ status: false, message: "Please provide folder name!" });
-    
+
     const check_one_folder = await find_folders({ user_id }, { user_id: 1, name: 1, parent_folder: 1 });
-    if (check_one_folder && check_one_folder.length){
+    if (check_one_folder && check_one_folder.length) {
       if (!parent_folder || !isValidObjectId(parent_folder)) return res.status(400).json({ status: false, message: "Please provide parent folder!" });
+
+      const parent_exists = await find_one_folder({ _id: parent_folder , user_id }, {});      
+      if (!parent_exists) return res.status(400).json({ status: false, message: "Parent folder does not exists!" });
+
+      const folder_exists = await find_one_folder({ name: folder_name, parent_folder: parent_folder, user_id }, {});
+      if (folder_exists) return res.status(400).json({ status: false, message: "Folder already exists!" });
     }
 
-    const folder_exists = await find_one_folder({ name: folder_name, parent_folder: parent_folder, user_id }, {});
-    if (folder_exists) return res.status(400).json({ status: false, message: "Folder already exists!" });
 
-    const add_folder = await add_new_folder({ name: folder_name, parent_folder: parent_folder ? parent_folder : null, user_id });
+    const add_folder = await add_new_folder({ name: folder_name, parent_folder: check_one_folder.length ? parent_folder : null, user_id });
     if (!add_folder) return res.status(400).json({ status: false, message: "Unable to create folder. Please try again later!" });
 
     return res.status(201).json({ status: true, message: "Folder created successfully" });
   } catch (error) {
-    console.log("error===>",error);
+    console.log("error===>", error);
     return res.status(400).json({ status: false, message: "Something went wrong!" });
   }
 };
@@ -34,10 +38,29 @@ exports.users_folders = async (req, res) => {
 
     return res.status(201).json({ status: true, data: folder_exists, message: "Folder fetched successfully" });
   } catch (error) {
-    console.log("error===>",error);
+    console.log("error===>", error);
     return res.status(400).json({ status: false, message: "Something went wrong!" });
   }
 };
+
+
+exports.get_one_folder = async (req, res) => {
+  try {
+    const user_id = req.user_id;
+    const { id } = req.params;
+    if (!isValidObjectId(id)) return res.status(400).json({ status: false, message: "Please provide folder ID!" });
+
+    const folder_exists = await find_one_folder({ _id: id, user_id });
+    if (!folder_exists) return res.status(400).json({ status: false, message: "Folder does not exists!" });
+
+    return res.status(201).json({ status: true,data:folder_exists, message: "Folder found successfully" });
+  } catch (error) {
+    console.log("error===>", error);
+    return res.status(400).json({ status: false, message: "Something went wrong!" });
+  }
+};
+
+
 
 exports.delete_folder = async (req, res) => {
   try {
@@ -53,7 +76,7 @@ exports.delete_folder = async (req, res) => {
 
     return res.status(201).json({ status: true, message: "Folder deleted successfully" });
   } catch (error) {
-    console.log("error===>",error);
+    console.log("error===>", error);
     return res.status(400).json({ status: false, message: "Something went wrong!" });
   }
 };
--- routes/folders.js
+++ routes/folders.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { AuthenticateApi } = require('../middlewares/auth');
-const { create_folder, users_folders, delete_folder } = require('../controllers/folder.controller');
+const { create_folder, users_folders, delete_folder, get_one_folder } = require('../controllers/folder.controller');
 const router = express.Router();
 
 router.use(AuthenticateApi)
 router.post("/", create_folder)
 router.get("/", users_folders)
+router.get("/:id", get_one_folder)
 router.delete("/:id", delete_folder)
 module.exports = router;
